0b28f1a04ba8806f3e12353e3232d926f71375c7
Vatsal Dave <ac121655@avcol.school.nz>
2023-03-15 12:13:32 +1300

    I converted a string to an int using the convert tool.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,28 @@
-﻿namespace maoriquiz_
+﻿using System;
+
+namespace maoriquiz_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string name;
+            int intnum;
+
+
             Console.WriteLine("Welcome to my Te Reo Maori Quiz!");
             Console.WriteLine("\nWhat is your name?");
             name = Console.ReadLine();
 
             Console.WriteLine($"What level would you like try "+name+"\n\n1.) Beginner\n2.) Intermediate\n3.) Advanced ?");
+            string strrnum = Console.ReadLine();
+            
+            intnum = Convert.ToInt32(strrnum);
+            
+            if (intnum == 1)
+            {
+
+            }
 
 
 
