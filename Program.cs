473543a12251e5e755e2d7ecec2f1ba552040455
Vatsal Dave <ac121655@avcol.school.nz>
2023-03-17 09:15:10 +1300

    I made my first three methods as  declaring them below as static void.

--- Program.cs
+++ Program.cs
@@ -9,23 +9,64 @@ namespace maoriquiz_
             string name;
             int intnum;
 
-
             Console.WriteLine("Welcome to my Te Reo Maori Quiz!");
             Console.WriteLine("\nWhat is your name?");
             name = Console.ReadLine();
 
-            Console.WriteLine($"What level would you like try "+name+"\n\n1.) Beginner\n2.) Intermediate\n3.) Advanced ?");
-            string strrnum = Console.ReadLine();
+            int num = 0;
+            Console.Clear();
+
+            Console.WriteLine($"What level would you like try "+name+"?\n\n1.) Beginner\n2.) Intermediate\n3.) Advanced");
+            string strnum = Console.ReadLine();
             
-            intnum = Convert.ToInt32(strrnum);
-            
+            intnum = Convert.ToInt32(strnum);
+
             if (intnum == 1)
             {
+                Beginner();
 
+            }
+
+            else if (intnum == 2)
+
+            {
+                Intermediate();
+            
+            }
+            
+            if (intnum == 3)
+
+            {
+                Advanced();
+            
             }
 
 
 
+
+
+
+
+
+
+
+            static void Beginner()
+            {
+               
+
+            }
+            
+            static void Intermediate()
+            {
+
+
+            }
+            
+            static void Advanced()
+            {
+
+
+            }
         }
     }
 }
