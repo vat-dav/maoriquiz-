faa57664a4deee7cd565b6c587977c4015bb229b
Vatsal Dave <ac121655@avcol.school.nz>
2023-03-20 22:26:33 +1300

    i finished coming up with the beginner questions and am thinking whether or not i can add it into a loop.

--- Program.cs
+++ Program.cs
@@ -77,20 +77,57 @@ namespace maoriquiz_
 
         static void BeginnerQuestions()
         {
-            string[] answers = {"aroha","kakariki","enoho", "awa",  };
-            string[] questions = {"\nQ1. What is love in Te Reo Maori?", 
-                                  "\nQ2. What is green in Te Reo Maori?", 
-                                  "\nQ3. What does the phrase 'sit down' mean in Te Reo Maori", 
-                                  "\nQ4. What is river in Te Reo Maori?",
-                                  "\nQ5. What is the name of the popular Maori dance (war cry) that rugby players do before they start their game?"};
+            string[] answers = {"aroha","kakariki","enoho", "awa", "haka", "waka", "pango or mango", "maunga","papa", "whaea"};
+
+            string[] questions = 
+                
+               {"\nQ1. What is 'love' in Te Reo Maori?",
+                "\nQ2. What is 'green' in Te Reo Maori?",
+                "\nQ3. What is the phrase 'sit down' in Te Reo Maori?",
+                "\nQ4. What is 'river' in Te Reo Maori?",
+                "\nQ5. What is the name of the popular Maori dance that rugby players do before they start their game?",
+                "\nQ6. What is the word for 'boat' in Te Reo Maori?",
+                "\nQ7. What is the colour 'black' in Te Reo Maori?",
+                "\nQ8. What is 'mountain' in Te Reo Maori?",
+                "\nQ9. What is 'father' in Te Reo Maori?",
+                "\nQ10. What is 'mother' in Te Reo Maori?"};
+
 
             Console.WriteLine(questions[0]);
+            Console.ReadLine();
+
+            if Console.ReadLine()= answers[1];
+
             Console.WriteLine(questions[1]);
+            Console.ReadLine();
+
             Console.WriteLine(questions[2]);
+            Console.ReadLine();
+
             Console.WriteLine(questions[3]);
+            Console.ReadLine();
+
             Console.WriteLine(questions[4]);
+            Console.ReadLine();
+
             Console.WriteLine(questions[5]);
-        
+            Console.ReadLine();
+
+            Console.WriteLine(questions[6]);    
+            Console.ReadLine();
+
+            Console.WriteLine(questions[7]);
+            Console.ReadLine();
+
+            Console.WriteLine(questions[8]);
+            Console.ReadLine();
+
+            Console.WriteLine(questions[9]);
+            Console.ReadLine();
+
+            Console.WriteLine(questions[10]);
+            Console.ReadLine();
+
         }
     }
 }
