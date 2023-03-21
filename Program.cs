a84defcc7359868100473a9474bd4212205ec755
Vatsal Dave <ac121655@avcol.school.nz>
2023-03-22 12:13:46 +1300

    instead of using an if statement nested in my for loop, I will use a nested do while loop as that will work much better.

--- Program.cs
+++ Program.cs
@@ -5,11 +5,11 @@ namespace maoriquiz_
 {
     internal class Program
     {
-        static string name, mark ;
+        static string name, mark;
         static void Main(string[] args)
         {
 
-           
+
 
             Console.WriteLine("Welcome to my Te Reo Maori Quiz!");
             Console.WriteLine("\nWhat is your name?");
@@ -29,8 +29,8 @@ namespace maoriquiz_
             Console.WriteLine($"\nWhat level would you like try " + name + "?\n\n1.) Beginner\n2.) Intermediate\n3.) Advanced");
             string level = Console.ReadLine();
             int intnum = Convert.ToInt32(level);
-        
-       
+
+
             if (intnum == 1)
             {
                 Beginner();
@@ -50,39 +50,18 @@ namespace maoriquiz_
                 Advanced();
 
             }
-            
+
         }
         static void Beginner()
         {
             Console.Clear();
             Console.WriteLine("you have chosen the Beginner level!");
 
-            BeginnerQuestions();
-        }
-
-        static void Intermediate()
-        {
-            Console.Clear();
-            Console.WriteLine("you have chosen the Intermediate level!");
-
-            IntermediateQuestions();
-        }
-
-        static void Advanced()
-        {
-            Console.Clear();
-            Console.WriteLine("you have chosen the Advanced level!");
-
-            AdvancedQuestions();
-        }
-
-        static void BeginnerQuestions()
-        {
             int[] n = new int[10];
-            string[] answers = {"a","b kakariki","a enoho", "c awa", "d haka", "a waka", "c and dpango or mango", "b maunga","a papa", "c whaea"};
+            string[] answers = { "a", "b kakariki", "a enoho", "c awa", "d haka", "a waka", "c and dpango or mango", "b maunga", "a papa", "c whaea" };
 
-            string[] questions = 
-                
+            string[] questions =
+
                {"Q1. What is 'love' in Te Reo Maori?\n\na.) aroha\nb.) tangata\nc.) pango\nd.) awa",
                 "Q2. What is 'green' in Te Reo Maori?",
                 "Q3. What is the phrase 'sit down' in Te Reo Maori?",
@@ -99,7 +78,7 @@ namespace maoriquiz_
             Console.WriteLine(questions[0]);
             string uanswers = Console.ReadLine();
 
-            for (int i = 0; i <= questions.Length; i++)
+            for (int i = 0; i < questions.Length; i++)
             {
                 if (uanswers == answers[i])
                 {
@@ -111,40 +90,31 @@ namespace maoriquiz_
                     Console.WriteLine("you're wrong");
 
 
-                }
-            }
-
-            static void IntermediateQuestions()
-            {
-                int[] n = new int[10];
-                string[] answers = { "", "", "", "", "", "", "", "", "", "" };
-
-                string[] questions =
-
-                   {"Q1. How do you say 'i love you' in Te Reo Maori?\n\na.) aroha ahau ki a koe\r\n\nb.) tangata ahau ki a koe\r\n\nc.) manaki aroha ahau koe\nd.) aroha ana ahau ki toku whanau\r\n",
-                "Q2. What is 'green' in Te Reo Maori?",
-                "Q3. What is the phrase 'sit down' in Te Reo Maori?",
-                "Q4. What is 'river' in Te Reo Maori?",
-                "Q5. What is the name of the popular Maori dance that rugby players do before they start their game?",
-                "Q6. What is the word for 'boat' in Te Reo Maori?",
-                "Q7. What is the colour 'black' in Te Reo Maori?",
-                "Q8. What is 'mountain' in Te Reo Maori?",
-                "Q9. What is 'father' in Te Reo Maori?",
-                "Q10. What is 'mother' in Te Reo Maori?"};
-
-                Console.Clear();
-
-                Console.WriteLine(questions[0]);
-                string uanswers = Console.ReadLine();
+                }//end of level method
 
             }
+        }
 
-            static void AdvancedQuestions()
-            {
-                Console.WriteLine("");
+        static void Intermediate()
+        {
+            Console.Clear();
+            Console.WriteLine("you have chosen the Intermediate level!");
 
-            }
+        }
+
+        static void Advanced()
+        {
+            Console.Clear();
+            Console.WriteLine("you have chosen the Advanced level!");
 
         }
     }
 }
+
+           
+                
+
+         
+
+        
+    
